dbf523c71c5190aec3bc6e945dc96574fd1e04db
lakiet1609 <kiettuanminhla1609@gmail.com>
2024-08-09 02:09:05 +0700

    update edge detection

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,1 +1,16 @@
 from pydantic_settings import BaseSettings
+
+class GeneralSettings(BaseSettings):
+    input_path: str = 'data'
+    output_path: str = 'results'
+
+
+class ShipDetectionSettings(BaseSettings):
+    gpu: int = 0
+    output_path: str = 'results/edge_detection'
+    checkpoint: str = 'models/bsds500_pascal_model.pth'
+
+
+general_settings = GeneralSettings()
+ship_detection_settings = ShipDetectionSettings()
+
